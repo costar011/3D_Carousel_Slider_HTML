8c7e63fa3dc342655bfe063c20149c04c259a8db
costar011 <dnaer456@gmail.com>
2024-08-21 23:01:42 +0900

    css&js

---
+++ script.js
@@ -0,0 +1,5 @@
+const imgSlider = document.querySelector('.imgSlider');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+let indexSlider = 0;
