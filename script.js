0bb54872f3931b156a052484fddfe2bdc6470efc
costar011 <dnaer456@gmail.com>
2024-08-22 21:31:06 +0900

    js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const nextBtn = document.querySelector('.next-btn');
 const prevBtn = document.querySelector('.prev-btn');
 
 let indexSlider = 0;
+
+nextBtn.addEventListener('click', () => {
+  indexSlider++;
+  imgSlider.style.transform = `rotate(${indexSlider * -90}deg)`;
+});
+
+prevBtn.addEventListener('click', () => {
+    indexSlider--;
+    imgSlider.style.transform = `rotate(${indexSlider * -90}deg)`;
+  });
