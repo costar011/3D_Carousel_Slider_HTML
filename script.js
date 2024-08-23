4177e436d833517b7485b228443538f8db8d3e22
costar011 <dnaer456@gmail.com>
2024-08-23 18:13:44 +0900

    js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const imgSlider = document.querySelector('.imgSlider');
+const imgFruits = document.querySelectorAll('.img-item.fruit');
+
+
 const nextBtn = document.querySelector('.next-btn');
 const prevBtn = document.querySelector('.prev-btn');
 
 let indexSlider = 0;
+let index = 0;
 
 nextBtn.addEventListener('click', () => {
   indexSlider++;
   imgSlider.style.transform = `rotate(${indexSlider * -90}deg)`;
+
+  index++;
+
+  imgFruits[index].classList.add('active');
 });
 
 prevBtn.addEventListener('click', () => {
