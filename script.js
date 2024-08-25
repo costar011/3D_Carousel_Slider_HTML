26c1b29568323329a7257b565e795d20e48cee72
costar011 <dnaer456@gmail.com>
2024-08-25 16:28:29 +0900

    js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const imgSlider = document.querySelector('.imgSlider');
-const imgFruits = document.querySelectorAll('.img-item.fruit');
+const imgSlider = document.querySelector(".img-slider");
+const imgFruits = document.querySelectorAll(".img-item.fruit");
 
-
-const nextBtn = document.querySelector('.next-btn');
-const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
 
 let indexSlider = 0;
 let index = 0;
 
-nextBtn.addEventListener('click', () => {
+nextBtn.addEventListener("click", () => {
   indexSlider++;
   imgSlider.style.transform = `rotate(${indexSlider * -90}deg)`;
 
   index++;
 
-  imgFruits[index].classList.add('active');
+  if (index > imgFruits.length - 1) {
+    index = 0;
+  }
+
+  document.querySelector(".fruit.active").classList.remove("active");
+  imgFruits[index].classList.add("active");
 });
 
-prevBtn.addEventListener('click', () => {
-    indexSlider--;
-    imgSlider.style.transform = `rotate(${indexSlider * -90}deg)`;
-  });
+prevBtn.addEventListener("click", () => {
+  indexSlider--;
+  imgSlider.style.transform = `rotate(${indexSlider * -90}deg)`;
+});
